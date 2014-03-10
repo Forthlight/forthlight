9c801384200cf977cba822ff206a15ab4dc80d9d
vagrant <vagrant@dev>
2014-03-10 12:51:07 +0000

    vagrant fix

--- vscripts/dev-depend.sh
+++ vscripts/dev-depend.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+sudo apt-get install -y curl
+\curl -L https://get.rvm.io | bash
+source /etc/profile
+rvm install ruby -v 2.1.0-p0
 
-#apache
-apt-get update
-apt-get install -y apache2
-rm -rf /var/www
-ln -fs /vagrant /var/www
-
-#git
 sudo apt-get install -y git
-sudo apt-get install -y git-flow
+
+sudo apt-get update
+sudo apt-get install -y apache2
 
 #mongo
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
@@ -16,10 +15,9 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 sudo apt-get update -y
 sudo apt-get install -y mongodb-10gen
 
-#ruby&rvm
-apt-get install -y curl
-\curl -L https://get.rvm.io | bash
-source /etc/profile
-#comment to install a specific rubyversion a launch, 1.9.3-p125 is an example
-rvm install ruby -v 2.1.0
-gem install rails -v 4.0.2
+sudo apt-get update -y
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y apache2-threaded-dev
+sudo apt-get install -y libapr1-dev
+sudo apt-get install -y libaprutil1-dev
+gem install rails -v 4.0.3
