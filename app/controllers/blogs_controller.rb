7044b0cc4150b9763b75722c7e10df45c9c83085
alxl <alx.l@gmx.com>
2014-05-09 11:33:27 +0000

    General fixes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,10 @@
 class BlogsController < ApplicationController
   def index
-    @blogs =  Administration::Blog.all.order("created_at DESC").page(params[:page]).per(10)
+    @blogs =  Administration::Blog.where(published: true).order("created_at DESC").page(params[:page]).per(10)
   end
 
   def show
     @blog = Administration::Blog.find(params[:id])
+    @recent = Administration::Blog.all.order("created_at DESC").limit(5)
   end
 end
