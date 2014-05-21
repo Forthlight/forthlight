3eff1b84d314c36032c109f3d9f342d2bc986c46
gray override <johan.b.damm@gmail.com>
2014-05-21 13:05:56 +0000

    capistarno struff

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -41,3 +41,16 @@ set :deploy_to, '~/www/stagelight/forthlight'
 #   }
 # setting per server overrides global ssh_options
 
+namespace :deploy do
+
+  desc "Restart application"
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute("pkill -f puma && startpuma")
+
+    end
+  end
+
+  after :finishing, "deploy:cleanup"
+
+end
