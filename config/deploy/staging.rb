23ce98d49729201895203ac2ca35ee33586937c6
gray override <johan.b.damm@gmail.com>
2014-05-28 12:38:10 +0000

    gemfile with stuff

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -47,7 +47,8 @@ namespace :deploy do
   desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute("pkill -f puma && startpuma")
+      execute("pkill -f puma && cd /home/jodg11/www/stagelight/forthlight/current && bundle exec puma -e staging -d -b unix:///tmp/stagelight.sock")
+
       puts "Server should now have restarted"
     end
   end
